2bde3f30326d63e81bc5fed38907e49e769f9766
lumlukwp <wasin_poncheewin@hotmail.com>
2025-12-21 17:19:14 +0700

    change web banner
    change web banner

--- app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>RodRaiDee – เปรียบเทียบต้นทุนรถ</title>
-      <meta name="description" content="Compare car costs easily" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  )
-}
